ba56a3d4cafbf8eb624fe12d12f149f1300862a4
Kojo Obeng-Arhin <KwadwoArhin@hotmail.com>
2021-03-26 18:57:27 -0400

    Added support for 3060TI.

--- bot.jsx
+++ bot.jsx
@@ -12,13 +12,19 @@ http.globalAgent.maxSockets = 1;
 https.globalAgent.maxSockets = 1;
 
 const url_test = 'https://www.bestbuy.ca/en-ca/product/apple-apple-lightning-to-3-5mm-headphone-jack-adapter-mmx62am-a/10487473';
-const url_3070 = 'https://www.bestbuy.ca/en-ca/product/nvidia-geforce-rtx-3070-8gb-gddr6-video-card-only-at-best-buy/15078017';
+const url_3060TI_NVIDIA = 'https://www.bestbuy.ca/en-ca/product/nvidia-geforce-rtx-3060-ti-8gb-gddr6-video-card/15166285'
+const url_3070_NVIDIA = 'https://www.bestbuy.ca/en-ca/product/nvidia-geforce-rtx-3070-8gb-gddr6-video-card-only-at-best-buy/15078017';
+const url_3070_EVGA = 'https://www.bestbuy.ca/en-ca/product/evga-geforce-rtx-3070-xc3-ultra-8gb-gddr6-video-card/15147122';
+const url_3070_ZOTAC = 'https://www.bestbuy.ca/en-ca/product/zotac-nvidia-geforce-rtx-3070-twin-edge-8gb-gddr6x-video-card/15000079';
+const url_3070_ASUS = 'https://www.bestbuy.ca/en-ca/product/asus-tuf-gaming-nvidia-geforce-rtx-3070-2x-oc-8gb-gddr6-video-card/15053087';
+const url_3070_MSI = 'https://www.bestbuy.ca/en-ca/product/msi-nvidia-geforce-rtx-3070-ventus-3x-oc-8gb-gddr6-video-card/15038016';
+const url_3070_ZOTAC_OC = 'https://www.bestbuy.ca/en-ca/product/zotac-nvidia-geforce-rtx-3070-twin-edge-oc-8gb-gddr6x-video-card/15000078';
 const url_cart = 'https://www.bestbuy.ca/en-ca/basket'
 const url_paypal = "https://www.bestbuy.ca/checkout/?qit=1#/en-ca/shipping/ON/M2N?expressPaypalCheckout=true"
 
-var checkInterval = setInterval(() => {checkStatus(url_3070)}, 5000);
 
-chromeOptions = {
+
+var chromeOptions = {
   headless: false,
   executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
   browserWSEndpoint: process.env.WS_ENDPOINT,
@@ -46,10 +52,18 @@ var mailOptions = {
   text: url_test
 };
 
+function intervalManager(flag, link, time=3000) {
+  var interval_id = link + "-interval"
 
+  if (flag) global[interval_id] = setInterval(() => {checkStatus(link)}, time)
+  else {
+    clearInterval(global[interval_id])
+  }
+}
 
 async function checkStatus (curr_url) {
-  clearInterval(checkInterval)
+
+  intervalManager(false, curr_url)
 
   var searchOptions = {
     uri: curr_url,
@@ -64,13 +78,15 @@ async function checkStatus (curr_url) {
   .then((html) => {
     var root = HTMLParser.parse(html)
     var availability_text = root.querySelector(".shippingAvailability_2RMa1").textContent
-    console.log(availability_text)
-    if (availability_text != "Coming soon") {
+    var gpu_type = curr_url.split("/")[5].split("-", 6)
+    console.log(gpu_type[3] + "-"  + gpu_type[4] + '  -------   ' + availability_text)
+
+    if (availability_text != "Coming soon" && availability_text != "Sold out online")   {
       sendEmail(curr_url)
       openBrowser(curr_url)
     }
     else {
-      var checkInterval = setInterval(() => {checkStatus(url_3070)}, 3000);
+      intervalManager(true, curr_url, 3000)
     }
   })
   .catch((error) => {
@@ -90,7 +106,7 @@ async function openBrowser (curr_url) {
       await page.deleteCookie({
         name : cookie.name,
         domain : cookie.domain,
-    })
+      })
     }
   }
 
@@ -107,31 +123,23 @@ async function openBrowser (curr_url) {
   await page.waitForSelector("#cvv")
   await page.type('#cvv', process.env.CVV)
 
-
+ // Paypal
   // await page.click('#signIn > div > button')
   // const response = await page.goto("https://www.bestbuy.ca/checkout/?qit=1#/en-ca/payment")
- 
-
-
   // await page.waitForSelector('#payment-submit-btn', { visible: true, timeout: 0 })
   // await page.click("#payment-submit-btn")
    // page.waitForNavigation({
     //   waitUntil: 'networkidle0'
-
-  
   // await page.waitForSelector("#root > div > div.x-page-content.container_3Sp8P > div.loader_3thnw > div.loadedContent_2Wp84 > section > div > section > section.cost-sum-section_3pPEp > div.checkoutOptions_1VB1L > div:nth-child(1) > div > div > a")
   // await page.click("#root > div > div.x-page-content.container_3Sp8P > div.loader_3thnw > div.loadedContent_2Wp84 > section > div > section > section.cost-sum-section_3pPEp > div.checkoutOptions_1VB1L > div:nth-child(1) > div > div > a")
   // await page.waitForSelector('#payment-submit-btn')
   // await page.click('#payment-submit-btn')
   // await page.focus("#root > div > div.x-page-content.container_3Sp8P > div.loader_3thnw > div.loadedContent_2Wp84 > section > div > section > section.cost-sum-section_3pPEp > div:nth-child(3) > div > a")
-  
-
   // await page.waitForSelector("#test > button")
   // await page.click("#test > button")
 }
 
 const sendEmail = async (curr_url) => {
-  clearInterval(checkInterval)
   mailOptions.text = curr_url; 
   let info = await transporter.sendMail(mailOptions, function(error, info){
     if (error) {
@@ -143,9 +151,7 @@ const sendEmail = async (curr_url) => {
 } 
 
 
-async function clearBrowser (page) {
-  // clear cookies
-  // const client = await page.target().createCDPSession()		
-  // await await client.send('Network.clearBrowserCookies')
-  console.log(page)
-}
+
+intervalManager(true, url_3070_NVIDIA, 3000)
+intervalManager(true, url_3060TI_NVIDIA, 3000)
+intervalManager(true, url_test, 3000)
